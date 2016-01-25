1c92f2e624a70a3f710bc46c59d3b2c4049045e8
Twisneski <tina.wisneski525@gmail.com>
2016-01-25 15:07:31 -0600

    added package.json and template-strings.js test file

---
+++ tests/template-strings.js
@@ -0,0 +1,26 @@
+var expect = require('chai').expect;
+
+describe('template strings', function () {
+  it('should be string', function () {
+   var string ='abc';
+
+    expect(string).to.be.a('string');
+  });
+
+  it('can evaluate variables', function () {
+    var bar = 'baz';
+    var string = 'foo${bar}';
+
+    expect(string).to.equal('foobaz');
+  });
+  it('can execute functions', function () {
+    var string = 'The date is: ${new Date()}'
+    var date = new Date();
+
+    expect(string).to.equal('The date is: '+ date);
+  });
+});
+
+
+
+
